7aaca5e8d2d5a41eb8cc74e7c6034b6b26bdce7b
Akshay Srivatsan <akshay01@stanford.edu>
2018-04-12 09:32:10 -0700

    create application page, hardcode questions

--- registration.js
+++ registration.js
@@ -1,12 +1,15 @@
+"use strict";
 var BASE_URL = 'https://madras-test.herokuapp.com';
 var LOGIN_URL = BASE_URL + '/login/';
 var SIGNUP_URL = BASE_URL + '/registration/signup/';
 var APPLICATION_URL = BASE_URL + '/application/';
 
+var questions;
 
 function login(e) {
     if (e) e.preventDefault();
     form = $('#login_form');
+    form.children('input').prop('disabled', true);
     params = {
         username: form.children('input[name=email]').val(),
         password: form.children('input[name=password]').val(),
@@ -27,6 +30,7 @@ function login(e) {
             $('#login_email_error').text(errors.username);
             $('#login_password_error').text(errors.password);
             $('#login_error').text(errors.non_field_errors);
+            form.children('input').prop('disabled', false);
         });
 }
 
@@ -96,17 +100,7 @@ function load_application() {
     $('#login_registration').hide();
     $('#application_form').show();
 
-    $.ajax({
-        method: "GET",
-        url: APPLICATION_URL,
-        headers: {
-            'Authorization': 'Token ' + localStorage.getItem('token')
-        }
-    }).done(function(data) {
-        console.log(data);
-    }).fail(function(data) {
-        console.log(data);
-    })
+    load_questions(profile_view);
 }
 
 function figure_out_current_view() {
@@ -136,3 +130,96 @@ function figure_out_current_view() {
 
 $(window).on('hashchange', figure_out_current_view);
 figure_out_current_view();
+
+function Question(name, type, required, label, max_length) {
+    this.question_name = name;
+    this.question_type = type;
+    this.question_required = required;
+    this.question_label = label;
+    this.question_max_length = max_length;
+
+    var wrapper = $("<div>");
+    wrapper.addClass("wrapper app question_" + type);
+    
+    var label = $("<label>");
+    label.addClass("question_label app");
+    label.prop('for', name);
+    label.text(this.question_label);
+    wrapper.append(label);
+
+    var input = $("<input>");
+    if (type == 'string') {
+        if (max_length <= 100) {
+            input.prop('type', 'text');
+            input.prop('maxlength', max_length || 524288);
+            this.category = 'profile';
+        } else {
+            wrapper.addClass('essay');
+            input = $('<textarea>');
+            this.category = 'application';
+        }
+    }
+    input.prop('placeholder', 'Your answer');
+    input.prop('id', name);
+    input.addClass('app');
+    wrapper.append(input);
+
+    this.container = wrapper;
+}
+
+function load_questions(cb) {
+    var questions_json = {
+        "github_username": {
+            "type": "string",
+            "required": false,
+            "read_only": false,
+            "label": "Github username",
+            "max_length": 39
+        },
+        "what_matters": {
+            "type": "string",
+            "required": false,
+            "read_only": false,
+            "label": "What matters to you and why?",
+            "max_length": 500
+        }
+    }
+
+    questions = [];
+
+    for (var key in questions_json) {
+        var q = new Question(key, questions_json[key]['type'], questions_json[key]['required'], questions_json[key]['label'], questions_json[key]['max_length']);
+        questions.push(q);
+    }
+
+    questions.map(function(q) {
+        if (q.category == 'profile') {
+            $('#personal_info').append(q.container);
+        } else if (q.category == 'application') {
+            $('#essays').append(q.container);
+        }
+    });
+
+    cb();
+}
+
+function profile_view(e) {
+    if (e) e.preventDefault();
+    $('#personal_info').show();
+    $('#essays').hide();
+    $('#page_title').text('profile');
+    $('#next_page').show();
+    $('#previous_page').hide();
+}
+
+function apply_view(e) {
+    if (e) e.preventDefault();
+    $('#personal_info').hide();
+    $('#essays').show();
+    $('#page_title').text('apply');
+    $('#next_page').hide();
+    $('#previous_page').show();
+}
+
+$('#next_page').click(apply_view);
+$('#previous_page').click(profile_view);
